b77b262e9dcf75aaedf8925126157216822c0b2f
Rafal <rafek131@wp.pl>
2025-03-01 20:48:53 +0100

    added products pictures

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtPage />
+		<NuxtRouteAnnouncer />
+		<NuxtWelcome />
+	</div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<h1>Products list</h1>
+</template>
+
+<style lang="scss" scoped></style>
