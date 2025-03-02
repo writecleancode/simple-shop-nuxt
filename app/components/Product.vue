670356fdced4bd7241ba9fc78364364139c449da
Rafal <rafek131@wp.pl>
2025-03-02 21:36:16 +0100

    Products are now dynamically displayed; updated images path

--- app/components/Product.vue
+++ app/components/Product.vue
@@ -1,8 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+	productData: {
+		id: string;
+		name: string;
+		category: string;
+		price: string;
+		imageUrl: string;
+	};
+}>();
+</script>
 
 <template>
-	<div>single product</div>
-	<img src="~/assets/images/becool.webp" alt="" />
+	<div>
+		<img :src="productData.imageUrl" :alt="productData.name" />
+		<span>{{ productData.name }}</span>
+		<span>{{ productData.category }}</span>
+		<span>{{ productData.price }}</span>
+	</div>
 </template>
 
 <style lang="scss" scoped></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { products } from '~/assets/data/products';
+</script>
 
 <template>
 	<div class="wrapper">
-		<h1>Products list</h1>
+		<h1>Products list:</h1>
 		<ul>
-			<li><Product /></li>
-			<li><Product /></li>
-			<li><Product /></li>
+			<li v-for="product in products" :key="product.id">
+				<Product :product-data="product" />
+			</li>
 		</ul>
 	</div>
 </template>
